3687374bf1ffdd2cf66a650593f61a906295cb23
Douglas Catchpole <douglas.catchpole@gmail.com>
2023-04-14 14:37:45 +1000

    fix: season filtered view.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,13 +3,17 @@
 <h3>Games</h3>
 <ul>
 @foreach ($games as $game)
-    <li><a href="{{ route('game', ['game' => $game->id]) }}">{{ $game->away_team->name }} @ {{ $game->home_team->name }}</a></li>
+    @if ($game->home_team->season === $season)
+        <li><a href="{{ route('game', ['game' => $game->id]) }}">{{ $game->away_team->name }} @ {{ $game->home_team->name }}</a></li>
+    @endif
 @endforeach
 </ul>
 <h3>Teams</h3>
 <ul>
 @foreach ($teams as $team)
-    <li><a href="{{ route('team', ['team' => $team->id]) }}">{{ $team->name }}</a></li>
+    @if ($team->season === $season)
+        <li><a href="{{ route('team', ['team' => $team->id]) }}">{{ $team->name }}</a></li>
+    @endif
 @endforeach
 </ul>
 @endforeach
