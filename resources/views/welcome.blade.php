d744a494a24dbfe7605fe39e85829505ffaa1b83
Douglas Catchpole <douglas.catchpole@gmail.com>
2024-08-18 04:26:57 +1000

    fix: missed close A tag.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@ Welcome
                 <li><a href="{{ route('game.view', ['game' => $game->id]) }}">{{ $game->away_team->name }} @ {{ $game->home_team->name }}</a> ({{ $game->firstPitch }})</li>
             @endif
         @endforeach
-            <li><a href="{{ route('game.create', ['season' => $season]) }}">New Game</li>
+            <li><a href="{{ route('game.create', ['season' => $season]) }}">New Game</a></li>
         </ul>
         <h3>Teams</h3>
         <ul>
