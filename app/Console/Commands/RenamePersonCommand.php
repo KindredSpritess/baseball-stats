bd2103e7e5c83e8cbbdeebe35c1038beed5438d3
Douglas Catchpole <douglas.catchpole@gmail.com>
2025-11-16 12:53:39 +1000

    feat: commands for fixing name mistakes.

---
+++ app/Console/Commands/RenamePersonCommand.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Person;
+use App\Models\Play;
+use Illuminate\Console\Command;
+
+class RenamePersonCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'person:rename {person_id : The ID of the person to rename} {new_first_name : The new first name} {new_last_name : The new last name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Rename a person and update all associated plays';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $personId = $this->argument('person_id');
+        $newFirstName = $this->argument('new_first_name');
+        $newLastName = $this->argument('new_last_name');
+
+        $person = Person::find($personId);
+
+        if (!$person) {
+            $this->error("Person with ID {$personId} not found.");
+            return 1;
+        }
+
+        $oldFirstName = $person->firstName;
+        $oldLastName = $person->lastName;
+        $oldName = "{$oldLastName}, {$oldFirstName}";
+
+        // Update the person
+        $person->update([
+            'firstName' => $newFirstName,
+            'lastName' => $newLastName,
+        ]);
+
+        $this->info("Updated person {$personId} from {$oldFirstName} {$oldLastName} to {$newFirstName} {$newLastName}");
+
+        // Update plays
+        $plays = Play::where('play', 'like', "%{$oldName}%")->get();
+
+        foreach ($plays as $play) {
+            $newPlay = preg_replace('/(?<!\w)' . preg_quote($oldName, '/') . '(?!\w)/', "{$newLastName}, {$newFirstName}", $play->play);
+            if ($newPlay !== $play->play) {
+                $play->update(['play' => $newPlay]);
+            }
+        }
+
+        $this->info("Updated " . $plays->count() . " plays.");
+
+        return 0;
+    }
+}
