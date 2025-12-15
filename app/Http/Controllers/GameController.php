2e8511b0ff1e3a6d7e0d37ec8b077445e5526561
Douglas Catchpole <douglas.catchpole@gmail.com>
2025-12-16 06:49:54 +1000

    feat: load ball in play into event.

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -97,6 +97,7 @@ class GameController extends Controller
 
         $gs = new GameState;
         $state = json_decode($gs->set($game, '', '', []), true);
+        $play->load('ballInPlay');
         GameUpdated::dispatch($game->id, [...$play->toArray(), 'actions' => $play->actions], $state, null);
         if ($json) {
             return new JsonResponse(['status' => 'success', 'state' => $state]);
